cc9f1c891ecf69836c5a0ce6b44e36200b3d91e4
Manuel Mazzuola <manuel.mazzuola@liberologico.com>
2014-01-28 09:34:37 +0100

    Added disable bluetoot on idle

--- speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/MainActivity.java
+++ speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/MainActivity.java
@@ -1,115 +1,55 @@
 package com.manuelmazzuola.speedtogglebluetooth;
 
-import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.CompoundButton;
 
-import com.manuelmazzuola.speedtogglebluetooth.fragment.MainFragment;
 import com.manuelmazzuola.speedtogglebluetooth.service.MonitorSpeed;
 
 /**
  * @author Manuel Mazzuola
  */
-public class MainActivity extends FragmentActivity {
-    private static Boolean bluetoothWasEnabled;
-    private final String DEVICES = "devices";
+public class MainActivity extends Activity {
     private Context ctx;
-    private final BluetoothAdapter bluetoothAdapter =
-            BluetoothAdapter.getDefaultAdapter();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle savedInstance) {
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainFragment())
-                    .commit();
+        Bundle extras;
+        String haveToStop = "";
+        if (savedInstance == null) {
+            extras = getIntent().getExtras();
+            if (extras != null)
+                haveToStop = extras.getString("close");
+
+        } else {
+            haveToStop = (String) savedInstance.getSerializable("close");
         }
+
+        if(haveToStop != null && haveToStop.equals("close")) {
+            stopService(new Intent(getApplicationContext(), MonitorSpeed.class));
+            finish();
+        }
+
+        super.onCreate(savedInstance);
+        ctx = this.getApplicationContext();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    public void onStart() {
+        super.onStart();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) { // Settings activity
-            bluetoothWasEnabled = bluetoothAdapter.isEnabled();
-            if(!bluetoothWasEnabled) {
-                registerForBluetoothChanges();
-                bluetoothAdapter.enable();
-            }else {
-                startSettingsActivity();
-            }
-            return true;
+        boolean isServiceRunning = MonitorSpeed.getRunning();
+
+        if (!isServiceRunning) {
+            startService(new Intent(ctx, MonitorSpeed.class));
         }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        ctx = getApplicationContext();
-        if(bluetoothWasEnabled != null && !bluetoothWasEnabled) {
-            bluetoothAdapter.disable();
-        }
+        finish();
     }
-
-    private void registerForBluetoothChanges() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-        registerReceiver(callStartSettingsActivityWhenBluetoothIsEnabled, filter);
-    }
-
-    private void startSettingsActivity() {
-        Intent settingsActivity = new Intent(ctx, SettingsActivity.class);
-        startActivity(settingsActivity);
-    }
-
-    public void onToggleButton(View view) {
-        CompoundButton buttonView = (CompoundButton) view;
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(ctx);
-
-        if(pref != null && pref.getString(DEVICES, "").equals("")) {
-            buttonView.toggle();
-            Utils.showAlert(this,
-                    "Please, select a device from settings page before start the service.",
-                    "Device missing");
-        }else if (buttonView.isChecked()) {
-            startService(new Intent(ctx, MonitorSpeed.class));
-        } else {
-            stopService(new Intent(ctx, MonitorSpeed.class));
-        }
-    }
-
-    private final BroadcastReceiver
-        callStartSettingsActivityWhenBluetoothIsEnabled = new BroadcastReceiver() {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
-                BluetoothAdapter.ERROR);
-
-        if (state == BluetoothAdapter.STATE_ON) {
-            unregisterReceiver(callStartSettingsActivityWhenBluetoothIsEnabled);
-            startSettingsActivity();
-        }
-        }
-    };
 }
--- speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/service/MonitorSpeed.java
+++ speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/service/MonitorSpeed.java
@@ -10,13 +10,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 
 import com.manuelmazzuola.speedtogglebluetooth.MainActivity;
 import com.manuelmazzuola.speedtogglebluetooth.R;
@@ -25,26 +25,163 @@ import com.manuelmazzuola.speedtogglebluetooth.R;
  * @author Manuel Mazzuola
  */
 public class MonitorSpeed extends Service implements LocationListener {
-    private int serviceReturnFlag = START_NOT_STICKY;
-    private final String DEVICES = "devices";
     private final int intentId = 1988;
     private final float triggerDistance = 260f;
     private Location oldLocation;
     private LocationManager lm;
     private String provider;
-    private static Boolean activated = Boolean.FALSE;
-    private static Boolean running = Boolean.FALSE;
+    private CountDownTimer countDownTimer;
+    private String DEFAULT_TITLE;
+    private String DEFAULT_MESSAGE;
+    private String WAITING_MESSAGE;
+    private int disabledCount = 0;
+    private static boolean activated = false;
+    private static boolean disabled = false;
+    private static boolean running = false;
 
     @Override
     public void onLocationChanged(Location location) {
         float distance = oldLocation.distanceTo(location);
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (distance >= triggerDistance && !activated) {
+        // Disabled is used when no device is available to connect
+        if (distance >= triggerDistance && !activated && !disabled) {
             bluetoothAdapter.enable();
             activated = true;
+            changeMessage(WAITING_MESSAGE);
+            starTimer();
+        }else if(distance < triggerDistance && !activated && disabled) {
+            if ((++disabledCount) > 2)
+                disabled = false;
         }
+
         oldLocation = location;
     }
+
+    @Override
+    public void onCreate() {
+        DEFAULT_TITLE  = this.getString(R.string.app_name);
+        DEFAULT_MESSAGE = this.getString(R.string.service_is_running);
+        WAITING_MESSAGE = this.getString(R.string.waiting_device);
+        activated = BluetoothAdapter.getDefaultAdapter().isEnabled();
+        running = Boolean.FALSE;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        running = Boolean.TRUE;
+
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        provider = lm.NETWORK_PROVIDER;
+        oldLocation = lm.getLastKnownLocation(provider);
+
+        IntentFilter filters = new IntentFilter();
+        // When to turn off bluetooth
+        filters.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        // When hold bluetooth on
+        filters.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        // When user directly turn on or off bluetooth
+        filters.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+
+        registerReceiver(bluetoothListener, filters);
+        lm.requestLocationUpdates(provider, 45 * 1000, 0f, this);
+
+        Intent stopIntent = new Intent(this, MainActivity.class);
+        stopIntent.putExtra("close", "close");
+        PendingIntent stopPendingIntent =
+                PendingIntent.getActivity(this, 0, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder note =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setContentTitle(DEFAULT_TITLE)
+                        .setContentText(DEFAULT_MESSAGE)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setAutoCancel(true)
+                        .setContentIntent(stopPendingIntent)
+                        .setSmallIcon(R.drawable.ic_action_bluetooth)
+                ;
+
+        note.getNotification().flags |= Notification.FLAG_AUTO_CANCEL;
+
+        Notification notification = note.build();
+        notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+
+        startForeground(intentId, note.build());
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        lm.removeUpdates(this);
+        unregisterReceiver(bluetoothListener);
+
+        running = Boolean.FALSE;
+        stopForeground(true);
+
+        super.onDestroy();
+    }
+
+    public static boolean getRunning() {
+        return running;
+    }
+
+    private void starTimer() {
+        countDownTimer = new CountDownTimer(60000, 60000) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                changeMessage(DEFAULT_MESSAGE);
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                bluetoothAdapter.disable();
+            }
+        }.start();
+    }
+
+    private void stopTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            countDownTimer = null;
+        }
+    }
+
+    private void changeMessage(String message) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder note =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle(DEFAULT_TITLE)
+                        .setContentText(message)
+                        .setSmallIcon(R.drawable.ic_action_bluetooth);
+
+        Notification notification = note.build();
+        notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(intentId, notification);
+    }
+
+    private final BroadcastReceiver bluetoothListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (activated && intent.getAction().equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+                activated = false;
+                disabled = false;
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                bluetoothAdapter.disable();
+            } else if (activated && intent.getAction().equals(BluetoothDevice.ACTION_ACL_CONNECTED)) {
+                disabled = false;
+                stopTimer();
+                changeMessage(DEFAULT_MESSAGE);
+            }else if (activated && intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF) {
+                disabledCount = 0;
+                disabled = true;
+                activated = false;
+            }
+
+            oldLocation = lm.getLastKnownLocation(provider);
+        }
+    };
 
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
@@ -62,91 +199,4 @@ public class MonitorSpeed extends Service implements LocationListener {
     @Override
     public void onProviderEnabled(String provider) {
     }
-
-    @Override
-    public void onCreate() {
-        activated = BluetoothAdapter.getDefaultAdapter().isEnabled();
-        running = Boolean.FALSE;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (!running) {
-            running = Boolean.TRUE;
-
-            lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            provider = lm.NETWORK_PROVIDER;
-            oldLocation = lm.getLastKnownLocation(provider);
-
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-            filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-
-            registerReceiver(bluetoothDeviceDisconnected, filter);
-            lm.requestLocationUpdates(provider, 45 * 1000, 0f, this);
-
-            Notification note = new Notification(
-                    R.drawable.ic_action_bluetooth,
-                    "Starting Speed Toggle bluetooth...",
-                    System.currentTimeMillis());
-            Intent noteIntent = new Intent(this, MainActivity.class);
-            noteIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-            PendingIntent pi = PendingIntent.getActivity(this, 0, noteIntent, 0);
-            note.setLatestEventInfo(this,
-                    "Speed Toggle Bluetooth",
-                    "Service is running",
-                    pi);
-            note.flags |= Notification.FLAG_NO_CLEAR;
-
-            startForeground(intentId, note);
-        }
-
-        return serviceReturnFlag;
-    }
-
-    @Override
-    public void onDestroy() {
-        lm.removeUpdates(this);
-        unregisterReceiver(bluetoothDeviceDisconnected);
-
-        running = Boolean.FALSE;
-        stopForeground(true);
-
-        super.onDestroy();
-    }
-
-    public static boolean getRunning() {
-        return running.booleanValue();
-    }
-
-    private final BroadcastReceiver bluetoothDeviceDisconnected = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (activated && intent.getAction().equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
-                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceAddress = "";
-                if (device != null) {
-                    deviceAddress = device.getAddress();
-                }
-
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                if (pref != null && pref.getString(DEVICES, "").equals(deviceAddress)) {
-                    bluetoothAdapter.disable();
-
-                    oldLocation = lm.getLastKnownLocation(provider);
-                    activated = false;
-                }
-            }else {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
-                        BluetoothAdapter.ERROR);
-                activated = state == BluetoothAdapter.STATE_ON;
-            }
-        }
-
-    };
 }
