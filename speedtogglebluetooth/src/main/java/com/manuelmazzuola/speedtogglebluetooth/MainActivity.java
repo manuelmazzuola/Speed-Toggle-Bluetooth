bac33f432416512f4d2708307363ba7cc0072d36
Manuel Mazzuola <manuel.mazzuola@liberologico.com>
2014-01-28 09:45:29 +0100

    Added license and turned off debug

--- speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/MainActivity.java
+++ speedtogglebluetooth/src/main/java/com/manuelmazzuola/speedtogglebluetooth/MainActivity.java
@@ -1,3 +1,22 @@
+/**
+ * Speed Toggle Bluetooth
+ * Copyright (C) 2014  Manuel Mazzuola
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.manuelmazzuola.speedtogglebluetooth;
 
 import android.app.Activity;
@@ -7,9 +26,6 @@ import android.os.Bundle;
 
 import com.manuelmazzuola.speedtogglebluetooth.service.MonitorSpeed;
 
-/**
- * @author Manuel Mazzuola
- */
 public class MainActivity extends Activity {
     private Context ctx;
 
